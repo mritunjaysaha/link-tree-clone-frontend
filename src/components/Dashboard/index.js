c43be0fe44ae3fbbd2cc906b1edbcbfc8168c57d
Mritunjay Saha <mritunjaysaha17@gmail.com>
2021-08-05 17:11:38 +0530

    made navbar responsive

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -14,7 +14,7 @@ export function Dashboard() {
                 get started for free
             </Link>
 
-            <p>
+            <p className={styles.loginContainer}>
                 Already have an account?{" "}
                 <span>
                     <Link to="/login" className={styles.login}>
