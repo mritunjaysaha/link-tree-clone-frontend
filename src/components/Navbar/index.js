a317b4264cccbb468e430d28730d5892f138a270
Mritunjay Saha <mritunjaysaha17@gmail.com>
2021-08-05 17:35:28 +0530

    added mobile navbar menu

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -5,13 +5,21 @@ export function Navbar() {
     return (
         <nav className={styles.nav}>
             <div className={styles.logo}>linktree</div>
-            <div>
-                <Link to="/login" className={styles.login}>
-                    Log in
-                </Link>
-                <Link to="/signup" className={styles.signup}>
-                    Sign Up Free
-                </Link>
+            <div className={styles.ctaContainer}>
+                <div>
+                    <Link to="/login" className={styles.login}>
+                        Log in
+                    </Link>
+                    <Link to="/signup" className={styles.signup}>
+                        Sign Up Free
+                    </Link>
+                </div>
+
+                <div className={styles.menu}>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
             </div>
         </nav>
     );
