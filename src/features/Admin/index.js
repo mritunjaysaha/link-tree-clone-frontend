86900a03bd37d65d9a559b0d7f1d9bb0f4199ce1
Mritunjay Saha <mritunjaysaha17@gmail.com>
2021-08-08 18:51:55 +0530

    removed admin panel

--- src/features/Admin/index.js
+++ src/features/Admin/index.js
@@ -1,14 +1,5 @@
-import { AdminSideNavbar } from "./Navbar/adminSideNavbar";
-import { UrlSection } from "./urlsSection";
-import { PreviewSection } from "./previewSection";
 import styles from "./admin.module.scss";
 
 export function Admin() {
-    return (
-        <section className={styles.adminContainer}>
-            <AdminSideNavbar />
-            <UrlSection />
-            <PreviewSection />
-        </section>
-    );
+    return <section className={styles.adminContainer}></section>;
 }
