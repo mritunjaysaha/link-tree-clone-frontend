1f14e77e655b790594254e9f3e02bc8397aa8fe5
Mritunjay Saha <mritunjaysaha17@gmail.com>
2021-08-11 10:52:29 +0530

    added url delete component

--- src/components/Admin/urls.jsx
+++ src/components/Admin/urls.jsx
@@ -2,12 +2,34 @@ import { useState, useRef } from "react";
 
 import styles from "./admin.module.scss";
 import { GoZap, GoKebabVertical } from "react-icons/go";
-import { MdDelete } from "react-icons/md";
-import { HiOutlinePhotograph } from "react-icons/hi";
 
-import { UilPen } from "@iconscout/react-unicons";
+import {
+    UilPen,
+    UilTrashAlt,
+    UilScenery,
+    UilTimes,
+} from "@iconscout/react-unicons";
+
+function UrlDeleteContainer({ handleDelete }) {
+    return (
+        <div className={styles.urlDeleteContainer}>
+            <div className={styles.deleteCloseDiv}>
+                <p>Delete</p>
+                <UilTimes className={styles.adminIcon} onClick={handleDelete} />
+            </div>
+            <div className={styles.deleteButtonsContainer}>
+                <p>Delete this forever?</p>
+                <div>
+                    <button type="button">cancel</button>
+                    <button type="button">delete</button>
+                </div>
+            </div>
+        </div>
+    );
+}
 
 function UrlItem() {
+    const [isDelete, setIsDelete] = useState(false);
     const titleRef = useRef();
     const urlRef = useRef();
 
@@ -19,60 +41,78 @@ function UrlItem() {
         urlRef.current.focus();
     }
 
+    function handleDelete() {
+        setIsDelete(!isDelete);
+    }
+
     return (
         <>
-            <div className={styles.urlItem}>
-                {/* draggable holder */}
-                <div className={styles.urlDrag}>
-                    <GoKebabVertical />
-                </div>
+            <section className={styles.urlItemSection}>
+                <div className={styles.urlItemDiv}>
+                    {/* draggable holder */}
+                    <div className={styles.urlDrag}>
+                        <GoKebabVertical />
+                    </div>
 
-                {/* url details */}
-                <div className={styles.urlDetails}>
-                    {/* url name */}
-                    <div className={styles.urlContents}>
-                        <input
-                            name="test"
-                            type="text"
-                            placeholder="Title"
-                            value="value"
-                            ref={titleRef}
-                        />
-                        <span>
-                            <UilPen
-                                className={styles.adminIcon}
-                                onClick={handleTitleClick}
+                    {/* url details */}
+                    <div className={styles.urlDetails}>
+                        {/* url name */}
+                        <div className={styles.urlContents}>
+                            <input
+                                name="test"
+                                type="text"
+                                placeholder="Title"
+                                value="value"
+                                ref={titleRef}
                             />
-                        </span>
-                        <div>{/* switch button */}</div>
-                    </div>
-                    {/* url link */}
-                    <div>
-                        <input
-                            type="text"
-                            placeholder="url"
-                            value="url"
-                            ref={urlRef}
-                        />
-                        <span>
-                            <UilPen
-                                className={styles.adminIcon}
-                                onClick={handleUrlClick}
-                            />
-                        </span>
-                    </div>
-                    <div className={styles.iconContainer}>
-                        <div>
-                            {/* icons */}
-                            <HiOutlinePhotograph className={styles.adminIcon} />
+                            <span>
+                                <UilPen
+                                    className={styles.adminIcon}
+                                    onClick={handleTitleClick}
+                                />
+                            </span>
+                            <div>{/* switch button */}</div>
                         </div>
+                        {/* url link */}
                         <div>
-                            {/* delete */}
-                            <MdDelete className={styles.adminIcon} />
+                            <input
+                                type="text"
+                                placeholder="url"
+                                value="url"
+                                ref={urlRef}
+                            />
+                            <span>
+                                <UilPen
+                                    className={styles.adminIcon}
+                                    onClick={handleUrlClick}
+                                />
+                            </span>
+                        </div>
+                        <div className={styles.iconContainer}>
+                            <div>
+                                {/* icons */}
+                                <UilScenery className={styles.adminIcon} />
+                            </div>
+                            <div>
+                                {/* delete */}
+                                <UilTrashAlt
+                                    id="delete-icon"
+                                    className={`${styles.adminIcon} ${
+                                        isDelete ? styles.adminIconActive : ""
+                                    }`}
+                                    onClick={handleDelete}
+                                />
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+                {/* Conditionally render delete component */}
+                {isDelete ? (
+                    <UrlDeleteContainer handleDelete={handleDelete} />
+                ) : (
+                    ""
+                )}
+            </section>
         </>
     );
 }
