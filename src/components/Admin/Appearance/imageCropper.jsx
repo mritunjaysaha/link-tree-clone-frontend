511ac49884678d5c4cb1d55ee1c55cde8dc3371b
Mritunjay Saha <mritunjaysaha17@gmail.com>
2021-08-17 18:35:28 +0530

    added imageCropper component

---
+++ src/components/Admin/Appearance/imageCropper.jsx
@@ -0,0 +1,26 @@
+import { useState, useCallback } from "react";
+import Cropper from "react-easy-crop";
+import styles from "./imageCropper.module.scss";
+
+export function ImageCropper({ image }) {
+    const [crop, setCrop] = useState({ x: 0, y: 0 });
+    const [zoom, setZoom] = useState(1);
+
+    const onCropComplete = useCallback((croppedArea, croppedAreaPixels) => {
+        console.log({ croppedArea, croppedAreaPixels });
+    }, []);
+
+    return (
+        <div className={styles.cropperContainer}>
+            <Cropper
+                image="https://img.huffingtonpost.com/asset/5ab4d4ac2000007d06eb2c56.jpeg?cache=sih0jwle4e&ops=1910_1000"
+                crop={crop}
+                zoom={zoom}
+                aspect={4 / 3}
+                onCropChange={setCrop}
+                onCropComplete={onCropComplete}
+                onZoomChange={setZoom}
+            />
+        </div>
+    );
+}
