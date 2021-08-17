b0e44b20ca5e2d90b3387cf18e0c4f2f49653ce3
Mritunjay Saha <mritunjaysaha17@gmail.com>
2021-08-17 17:08:52 +0530

    added image preview

--- src/components/Admin/Appearance/uploadModal.jsx
+++ src/components/Admin/Appearance/uploadModal.jsx
@@ -1,14 +1,35 @@
 import styles from "./uploadModal.module.scss";
 import { UilFilePlus, UilTimes, UilLaptop } from "@iconscout/react-unicons";
 import { useDropzone } from "react-dropzone";
-import { useCallback } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 export function UploadModal({ handleClose }) {
+    const [files, setFiles] = useState([]);
+
     const onDrop = useCallback((acceptedFiles) => {
-        console.log(acceptedFiles);
-    });
+        setFiles(
+            acceptedFiles.map((file) =>
+                Object.assign(file, {
+                    preview: URL.createObjectURL(file),
+                })
+            )
+        );
+    }, []);
 
     const { getRootProps, getInputProps } = useDropzone({ onDrop });
+
+    const thumbs = files.map((file) => (
+        <div className={styles.thumbsDiv} key={file.name}>
+            <img src={file.preview} alt={file.name} />
+        </div>
+    ));
+
+    useEffect(
+        () => () => {
+            files.forEach((file) => URL.revokeObjectURL(file.preview));
+        },
+        [files]
+    );
 
     return (
         <section className={styles.uploadModalContainer}>
@@ -29,9 +50,15 @@ export function UploadModal({ handleClose }) {
                 <div {...getRootProps()} className={styles.dragComponentOuter}>
                     <input {...getInputProps()} />
                     <div className={styles.dragComponentInner}>
-                        <UilFilePlus className={styles.fileIcon} />
-                        <h3>Select files to upload</h3>
-                        <p>or Drag and Drop, Copy and Paste Files</p>
+                        {thumbs ? (
+                            thumbs
+                        ) : (
+                            <>
+                                <UilFilePlus className={styles.fileIcon} />
+                                <h3>Select files to upload</h3>
+                                <p>or Drag and Drop, Copy and Paste Files</p>
+                            </>
+                        )}
                     </div>
                 </div>
             </section>
