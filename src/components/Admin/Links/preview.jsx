7b73aa17f07c47a5e7dc394946b57868ad0834d3
Mritunjay Saha <mritunjaysaha17@gmail.com>
2021-09-03 23:32:39 +0530

    added QR Code in the share modal

--- src/components/Admin/Links/preview.jsx
+++ src/components/Admin/Links/preview.jsx
@@ -1,18 +1,72 @@
 import { useState } from "react";
 import { useSelector } from "react-redux";
-import { UilTimes } from "@iconscout/react-unicons";
-
+import { UilTimes, UilQrcodeScan } from "@iconscout/react-unicons";
+import QRCode from "react-qr-code";
 import { UserViewContents } from "../../UserView";
 import linktree from "../../../assets/linktree.svg";
 
 import styles from "./preview.module.scss";
 
+function ShareModal({ handleShareModalClick, handleQRModalClicked, link }) {
+    return (
+        <>
+            <header>
+                <p>Share your linktree</p>
+                <div onClick={handleShareModalClick}>
+                    <UilTimes className="fontawesome-icon" />
+                </div>
+            </header>
+            <ul>
+                <li className={styles.QRLi} onClick={handleQRModalClicked}>
+                    <UilQrcodeScan className={styles.QRCode} />
+                    <p>QR Code</p>
+                </li>
+
+                <li>
+                    <div className={styles.copyLinkContainer}>
+                        <div>
+                            <img src={linktree} alt="linktree" />
+                            <p>{link}</p>
+                        </div>
+                        <button
+                            onClick={() => {
+                                navigator.clipboard
+                                    .writeText(link)
+                                    .then(() =>
+                                        console.log("text copied to clipboard")
+                                    )
+                                    .catch((err) =>
+                                        console.log(
+                                            "failed copying text to clipboard",
+                                            err.message
+                                        )
+                                    );
+                            }}
+                        >
+                            Copy
+                        </button>
+                    </div>
+                </li>
+            </ul>
+        </>
+    );
+}
+
 function PreviewNav() {
     const [isClicked, setIsClicked] = useState(false);
+    const [isQRCode, setIsQRCode] = useState(false);
 
     const { username } = useSelector((state) => state.user);
 
     const link = `https://link-tree-clone.vercel.app/${username}`;
+
+    function handleShareModalClick() {
+        setIsClicked(false);
+    }
+
+    function handleQRModalClicked() {
+        setIsQRCode(true);
+    }
 
     return (
         <>
@@ -32,45 +86,33 @@ function PreviewNav() {
             </nav>
             {isClicked && (
                 <div className={styles.shareModal}>
-                    <header>
-                        <p>Share your linktree</p>
-                        <div
-                            onClick={() => {
-                                setIsClicked(false);
-                            }}
-                        >
-                            <UilTimes className="fontawesome-icon" />
-                        </div>
-                    </header>
-                    <ul>
-                        <li>
-                            <div className={styles.copyLinkContainer}>
-                                <div>
-                                    <img src={linktree} alt="linktree" />
-                                    <p>{link}</p>
-                                </div>
-                                <button
+                    {!isQRCode ? (
+                        <ShareModal
+                            handleQRModalClicked={handleQRModalClicked}
+                            handleShareModalClick={handleShareModalClick}
+                            link={link}
+                        />
+                    ) : (
+                        <>
+                            <header>
+                                <div
                                     onClick={() => {
-                                        navigator.clipboard
-                                            .writeText(link)
-                                            .then(() =>
-                                                console.log(
-                                                    "text copied to clipboard"
-                                                )
-                                            )
-                                            .catch((err) =>
-                                                console.log(
-                                                    "failed copying text to clipboard",
-                                                    err.message
-                                                )
-                                            );
+                                        setIsQRCode(!isQRCode);
                                     }}
+                                ></div>
+                                <p>QR Code</p>
+                                <div
+                                    className={styles.QRCodeModal}
+                                    onClick={handleShareModalClick}
                                 >
-                                    Copy
-                                </button>
+                                    <UilTimes className="fontawesome-icon" />
+                                </div>
+                            </header>
+                            <div>
+                                <QRCode value={link} />
                             </div>
-                        </li>
-                    </ul>
+                        </>
+                    )}
                 </div>
             )}
         </>
