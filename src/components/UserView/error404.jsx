3dbc72a59500df391b0fd1431872f3dc74e6498c
Mritunjay Saha <mritunjaysaha17@gmail.com>
2021-09-14 14:46:40 +0530

    added 404 error page for userview

--- src/components/UserView/error404.jsx
+++ src/components/UserView/error404.jsx
@@ -1,9 +1,15 @@
 import styles from "./error404.module.scss";
+import errorIllustration from "../../assets/undraw_page_not_found_su7k.svg";
 
 export function ErrorPage() {
     return (
         <section className={styles.errorPageSection}>
-            404 user not found
+            <div className={styles.errorContainerDiv}>
+                <img src={errorIllustration} alt="404" />
+                <div>
+                    <p>user not found</p>
+                </div>
+            </div>
         </section>
     );
 }
